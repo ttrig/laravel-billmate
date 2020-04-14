2c0b889ca774233d421b3e2dceedaf8a3427aa0b
markus <markus.lilienberg@glesys.se>
2020-04-14 12:59:33 +0200

    Refactor ServiceProvider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,40 +7,40 @@ use Ttrig\Billmate\Middlewares\TransformRedirectRequest;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/billmate.php', 'billmate');
+    }
+
     public function boot()
     {
         $this->publishes([__DIR__ . '/../config/billmate.php' => config_path('billmate.php')], 'config');
 
         $this->loadViewsFrom(__DIR__ . '/views', 'billmate');
 
+        $this->registerRoutes();
+    }
+
+    public function registerRoutes()
+    {
         $routeConfig = [
             'prefix' => config('billmate.route_prefix'),
             'as' => 'billmate.',
+            'middleware' => [VerifyRequest::class],
         ];
 
         $this->app['router']->group($routeConfig, function ($router) {
             $router->post('accept', config('billmate.accept_action'))
                 ->name('accept')
-                ->middleware([
-                    TransformRedirectRequest::class,
-                    VerifyRequest::class,
-                ]);
+                ->middleware([TransformRedirectRequest::class]);
 
             $router->post('cancel', config('billmate.cancel_action'))
                 ->name('cancel')
-                ->middleware([
-                    TransformRedirectRequest::class,
-                    VerifyRequest::class,
-                ]);
+                ->middleware([TransformRedirectRequest::class]);
 
             $router->post('callback', config('billmate.callback_action'))
                 ->name('callback')
                 ->middleware(VerifyRequest::class);
         });
-    }
-
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__ . '/../config/billmate.php', 'billmate');
     }
 }
